1f0e538f8944f856a7cd5765f0336aac2cc19220
Joanne Raye <joanneraye8@gmail.com>
2018-06-27 12:30:23 -0400

    getting started with trying to get wcf access to work in ios.  No success yet.

--- Zipline2/Zipline2.iOS/AppDelegate.cs
+++ Zipline2/Zipline2.iOS/AppDelegate.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using Foundation;
+using Staunch.POS.Classes;
 using UIKit;
+using Zipline2.ConnectedServices;
 
 namespace Zipline2.iOS
 {
@@ -28,6 +31,29 @@ namespace Zipline2.iOS
 
             Plugin.Iconize.Iconize.With(new Plugin.Iconize.Fonts.FontAwesomeModule());
             FormsPlugin.Iconize.iOS.IconControls.Init();
+
+
+            //waiterClient = new PosServiceClient(
+            //         new BasicHttpBinding(),
+            //         new EndpointAddress("http://192.168.1.26/WP7Waiter/POServiceHost.svc"));
+            //waiterClient.Endpoint.Binding.SendTimeout = new TimeSpan(0, 10, 0);
+
+            //checkClient = new CheckHostClient(
+            //     new BasicHttpBinding(),
+            //     new EndpointAddress("http://192.168.1.26/CheckHost/CheckHost.svc"));
+            //checkClient.Endpoint.Binding.SendTimeout = new TimeSpan(0, 10, 0);
+
+            //ChannelFactory<IPosService> WaiterClientChannelFactory = new ChannelFactory<IPosService>(
+            //    new BasicHttpBinding(), 
+            //    new EndpointAddress("http://192.168.1.26/WP7Waiter/POServiceHost.svc"));
+
+            //IPosService waiterclient = WaiterClientChannelFactory.CreateChannel();
+            //DBUser user = waiterclient.GetUser("8011");
+
+
+            //ChannelFactory<Zipline2.ConnectedServices.ICheckHost> CheckClientChannelFactory = new ChannelFactory<ICheckHost>(
+            //    new BasicHttpBinding(),
+            //    new EndpointAddress("http://192.168.1.26/CheckHost/CheckHost.svc"));
 
             LoadApplication(new App());
 
